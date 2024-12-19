91d3b3157799ff73713c5c1c2ecf6b0f6db447a0
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2024-12-19 09:47:54 -0800

    start-3

--- CS50 Web/3-Django/lecture3/lecture3/settings.py
+++ CS50 Web/3-Django/lecture3/lecture3/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tasks',
     'newyear',
     'hello',
     'django.contrib.admin',
@@ -117,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
